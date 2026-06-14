376fd0bd57a4cdf95bea5496965a3689c3bbbb70
drakthephenomenal <akthephenomenal@zohomail.com>
2026-06-14 15:09:17 +0600

    Add files via upload

---
+++ names28.js
@@ -0,0 +1,38 @@
+/**
+ * names28.js — 28 Names of Radha Rani
+ * Separate data file for the Sri Harivangsa Bank app.
+ * Drop this file next to your index.html and include it with:
+ *   <script src="names28.js"></script>
+ * Then access: window.NAMES_28
+ */
+
+window.NAMES_28 = [
+  { num: "১",  name: "রাধা",                      nameHindi: "राधा",                    meaning: "The Supreme Beloved" },
+  { num: "২",  name: "রাসেশ্বরী",                 nameHindi: "रासेश्वरी",               meaning: "Goddess of the Rasa dance" },
+  { num: "৩",  name: "রম্যা",                     nameHindi: "रम्या",                   meaning: "The most beautiful & delightful" },
+  { num: "৪",  name: "শ্রীকৃষ্ণমন্ত্রাধিদেবতা",  nameHindi: "श्रीकृष्णमन्त्राधिदेवता", meaning: "Presiding deity of Krishna-mantra" },
+  { num: "৫",  name: "সর্বাদ্যা",                 nameHindi: "सर्वाद्या",               meaning: "The primordial, first of all" },
+  { num: "৬",  name: "সর্ববন্দ্যা",              nameHindi: "सर्वबन्द्या",             meaning: "Worthy of worship by all" },
+  { num: "৭",  name: "বৃন্দাবনবিহারিণী",         nameHindi: "वृन्दावनविहारिणी",       meaning: "Who plays in Vrindavan" },
+  { num: "৮",  name: "বৃন্দারাধ্যা",             nameHindi: "वृन्दाराध्या",            meaning: "Worshipped by Vrinda Devi" },
+  { num: "৯",  name: "রমা",                       nameHindi: "रमा",                     meaning: "The blissful one" },
+  { num: "১০", name: "অশেষগোপীমণ্ডলপূজিতা",     nameHindi: "अशेषगोपीमण्डलपूजिता",    meaning: "Worshipped by all the gopis" },
+  { num: "১১", name: "সত্যা",                     nameHindi: "सत्या",                   meaning: "The eternal Truth" },
+  { num: "১২", name: "সত্যপরা",                   nameHindi: "सत्यपरा",                 meaning: "Supreme among the truthful" },
+  { num: "১৩", name: "সত্যভামা",                  nameHindi: "सत्यभामा",                meaning: "True and lustrous one" },
+  { num: "১৪", name: "শ্রীকৃষ্ণবল্লভা",          nameHindi: "श्रीकृष्णवल्लभा",        meaning: "The beloved of Shri Krishna" },
+  { num: "১৫", name: "বৃষভানুসুতা",               nameHindi: "वृषभानुसुता",             meaning: "Daughter of King Vrishabhanu" },
+  { num: "১৬", name: "গোপী",                      nameHindi: "गोपी",                    meaning: "The divine cowherd girl" },
+  { num: "১৭", name: "মূলপ্রকৃতি",               nameHindi: "मूलप्रकृति",              meaning: "The primordial nature" },
+  { num: "১৮", name: "ঈশ্বরী",                   nameHindi: "ईश्वरी",                  meaning: "The supreme goddess" },
+  { num: "১৯", name: "গান্ধর্বা",                 nameHindi: "गान्धर्वा",               meaning: "Goddess of divine music" },
+  { num: "২০", name: "রাধিকা",                    nameHindi: "राधिका",                  meaning: "She who worships Krishna" },
+  { num: "২১", name: "আরম্যা",                    nameHindi: "आरम्या",                  meaning: "Noble, honoured one" },
+  { num: "২২", name: "রুক্মিণী",                  nameHindi: "रुक्मिणी",                meaning: "Adorned with gold" },
+  { num: "২৩", name: "পরমেশ্বরী",                 nameHindi: "परमेश्वरी",               meaning: "The supreme ruler" },
+  { num: "২৪", name: "পরাৎপরতরা",                 nameHindi: "परात्परतरा",               meaning: "Beyond the beyond" },
+  { num: "২৫", name: "পূর্ণা",                    nameHindi: "पूर्णा",                  meaning: "The complete, perfect one" },
+  { num: "২৬", name: "পূর্ণচন্দ্রনিভাননা",       nameHindi: "पूर्णचन्द्रनिभानना",      meaning: "Face like the full moon" },
+  { num: "২৭", name: "ভুক্তিমুক্তিপ্রদা",        nameHindi: "भुक्तिमुक्तिप्रदा",       meaning: "Giver of enjoyment & liberation" },
+  { num: "২৮", name: "ভবব্যাধিবিনাশিনী",          nameHindi: "भवव्याधिविनाशिनी",        meaning: "Destroyer of worldly suffering" },
+];
